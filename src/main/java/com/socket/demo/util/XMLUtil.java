061ca0ea61bc66f07be9002dec0e3aa2e0843441
sunytonyli <sunytonyli@gmail.com>
2018-03-05 21:37:51 +0800

    add string to xml

---
+++ src/main/java/com/socket/demo/util/XMLUtil.java
@@ -0,0 +1,36 @@
+package com.socket.demo.util;
+
+import java.io.StringReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+
+/**
+ * @author songyu.li
+ * 
+ */
+public class XMLUtil {
+
+		/**
+		 * 将String类型的xml转换成对象
+		 * @param clazz
+		 * @param xmlStr
+		 * @return
+		 */
+		public static Object convertXmlStrToObject(Class clazz, String xmlStr) {
+			Object xmlObject = null;
+			try {
+				JAXBContext context = JAXBContext.newInstance(clazz);
+				//进行将Xml转成对象的核心接口
+				Unmarshaller unmarshaller = context.createUnmarshaller();
+				StringReader sr = new StringReader(xmlStr);
+				xmlObject = unmarshaller.unmarshal(sr);
+			} catch (JAXBException e) {
+				e.printStackTrace();
+			}
+			return xmlObject;
+		}
+	
+}
